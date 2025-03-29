6c2fc9a5d0f9e65593e44064a0f16af8bc59eb2f
Dollyn-boy <vitordoreahdx11@gmail.com>
2025-03-29 18:08:29 -0300

    Ajudando meu mano montanha

--- src/model/Serie.java
+++ src/model/Serie.java
@@ -7,6 +7,31 @@ public class Serie extends MediaAudioVisual{
     private LocalDateTime dataFim;
     private List<Temporada> temporadas;
 
+    @Override
+    public int getPontuacao() {
+        return 0;
+    }
+
+    public LocalDateTime getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(LocalDateTime dataFim) {
+        this.dataFim = dataFim;
+    }
+
+    public List<Temporada> getTemporadas() {
+        return temporadas;
+    }
+
+    public void setTemporadas(List<Temporada> temporadas) {
+        this.temporadas = temporadas;
+    }
+
+    @Override
+    public void Avaliar(LocalDateTime dataAvaliacao, int pontuacao, String review, LocalDateTime dataConsumo) {
+
+    }
 }
 
 
